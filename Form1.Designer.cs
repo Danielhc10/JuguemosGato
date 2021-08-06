249e61dc59125e56f7636a0b57f0bd36acaee6e8
Daniel Hernandez <dani2203g@gmail.com>
2021-08-05 22:57:46 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace JuguemosGato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.playGame = new System.Windows.Forms.Button();
+            this.reinicio = new System.Windows.Forms.Button();
+            this.txtMichi = new System.Windows.Forms.Label();
+            this.txtPlayer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.namePlayer = new System.Windows.Forms.TextBox();
+            this.IA = new System.Windows.Forms.Timer(this.components);
+            this.btnX = new System.Windows.Forms.RadioButton();
+            this.btnO = new System.Windows.Forms.RadioButton();
+            this.botoncitos = new System.Windows.Forms.GroupBox();
+            this.botoncitos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Location = new System.Drawing.Point(77, 127);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(50, 50);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.playerClick);
+            // 
+            // a2
+            // 
+            this.a2.Location = new System.Drawing.Point(133, 127);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(50, 50);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.playerClick);
+            // 
+            // a3
+            // 
+            this.a3.Location = new System.Drawing.Point(189, 127);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(50, 50);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.playerClick);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(189, 183);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(50, 50);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.playerClick);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(133, 183);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(50, 50);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.playerClick);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(77, 183);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(50, 50);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.playerClick);
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(189, 239);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(50, 50);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.playerClick);
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(133, 239);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(50, 50);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.playerClick);
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(77, 239);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(50, 50);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.playerClick);
+            // 
+            // playGame
+            // 
+            this.playGame.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playGame.Location = new System.Drawing.Point(47, 371);
+            this.playGame.Name = "playGame";
+            this.playGame.Size = new System.Drawing.Size(106, 32);
+            this.playGame.TabIndex = 9;
+            this.playGame.Text = "Juguemos";
+            this.playGame.UseVisualStyleBackColor = true;
+            this.playGame.Click += new System.EventHandler(this.iniciarJuego);
+            // 
+            // reinicio
+            // 
+            this.reinicio.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reinicio.Location = new System.Drawing.Point(175, 371);
+            this.reinicio.Name = "reinicio";
+            this.reinicio.Size = new System.Drawing.Size(104, 32);
+            this.reinicio.TabIndex = 10;
+            this.reinicio.Text = "¡Otra vez!";
+            this.reinicio.UseVisualStyleBackColor = true;
+            this.reinicio.Click += new System.EventHandler(this.reiniciarJuego);
+            // 
+            // txtMichi
+            // 
+            this.txtMichi.AutoSize = true;
+            this.txtMichi.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMichi.Location = new System.Drawing.Point(171, 9);
+            this.txtMichi.Name = "txtMichi";
+            this.txtMichi.Size = new System.Drawing.Size(127, 20);
+            this.txtMichi.TabIndex = 11;
+            this.txtMichi.Text = "Michi wins - 0";
+            // 
+            // txtPlayer
+            // 
+            this.txtPlayer.AutoSize = true;
+            this.txtPlayer.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayer.Location = new System.Drawing.Point(12, 9);
+            this.txtPlayer.Name = "txtPlayer";
+            this.txtPlayer.Size = new System.Drawing.Size(134, 20);
+            this.txtPlayer.TabIndex = 12;
+            this.txtPlayer.Text = "Player wins - 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Juguemos Gato";
+            // 
+            // namePlayer
+            // 
+            this.namePlayer.Location = new System.Drawing.Point(110, 323);
+            this.namePlayer.Name = "namePlayer";
+            this.namePlayer.Size = new System.Drawing.Size(100, 20);
+            this.namePlayer.TabIndex = 14;
+            // 
+            // IA
+            // 
+            this.IA.Interval = 1000;
+            this.IA.Tick += new System.EventHandler(this.IAmoves);
+            // 
+            // btnX
+            // 
+            this.btnX.AutoSize = true;
+            this.btnX.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnX.Location = new System.Drawing.Point(6, 19);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(36, 21);
+            this.btnX.TabIndex = 15;
+            this.btnX.TabStop = true;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = true;
+            // 
+            // btnO
+            // 
+            this.btnO.AutoSize = true;
+            this.btnO.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnO.Location = new System.Drawing.Point(48, 19);
+            this.btnO.Name = "btnO";
+            this.btnO.Size = new System.Drawing.Size(36, 21);
+            this.btnO.TabIndex = 16;
+            this.btnO.TabStop = true;
+            this.btnO.Text = "O";
+            this.btnO.UseVisualStyleBackColor = true;
+            // 
+            // botoncitos
+            // 
+            this.botoncitos.Controls.Add(this.btnX);
+            this.botoncitos.Controls.Add(this.btnO);
+            this.botoncitos.Location = new System.Drawing.Point(12, 32);
+            this.botoncitos.Name = "botoncitos";
+            this.botoncitos.Size = new System.Drawing.Size(83, 40);
+            this.botoncitos.TabIndex = 17;
+            this.botoncitos.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 450);
+            this.Controls.Add(this.botoncitos);
+            this.Controls.Add(this.namePlayer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlayer);
+            this.Controls.Add(this.txtMichi);
+            this.Controls.Add(this.reinicio);
+            this.Controls.Add(this.playGame);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jueguemos Gato con Michi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.botoncitos.ResumeLayout(false);
+            this.botoncitos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button playGame;
+        private System.Windows.Forms.Button reinicio;
+        private System.Windows.Forms.Label txtMichi;
+        private System.Windows.Forms.Label txtPlayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox namePlayer;
+        private System.Windows.Forms.Timer IA;
+        private System.Windows.Forms.RadioButton btnX;
+        private System.Windows.Forms.RadioButton btnO;
+        private System.Windows.Forms.GroupBox botoncitos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuguemosGato
+{
+    public partial class Form1 : Form
+    {
+
+        public enum Player
+        {
+            X, O
+        }
+
+        Player jugadorActual;
+        List<Button> buttons;
+        Random rand = new Random();
+
+        string nombre = "";
+        
+        int playerWins = 0;
+        int IAWins = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetGame();
+            this.reinicio.Enabled = false;
+            this.btnO.Visible = false;
+            this.btnX.Visible = false;
+            this.botoncitos.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OnOffBtn(false);
+        }
+
+        private void OnOffBtn(bool OnOff)
+        {
+            a1.Enabled = OnOff;
+            a2.Enabled = OnOff;
+            a3.Enabled = OnOff;
+            b1.Enabled = OnOff;
+            b2.Enabled = OnOff;
+            b3.Enabled = OnOff;
+            c1.Enabled = OnOff;
+            c2.Enabled = OnOff;
+            c3.Enabled = OnOff;
+        }
+
+        private void playerClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender; // find which button was clicked
+            jugadorActual = Player.X; // set the player to X
+            button.Text = jugadorActual.ToString(); // change the button text to player X
+            button.Enabled = false; // disable the button
+            button.BackColor = System.Drawing.Color.Cyan; // change the player colour to Cyan
+            buttons.Remove(button); //remove the button from the list buttons so the AI can't click it either
+            Check(); // check if the player won
+            IA.Start(); // stop the AI timer
+        }
+        private void IAmoves(object sender, EventArgs e)
+        {
+            if (buttons.Count > 0)
+            {
+                int index = rand.Next(buttons.Count); // generate a random number within the number of buttons available
+                buttons[index].Enabled = false; // assign the number to the button
+                                                // when the random number is generated then we will look into the list
+                                                // for what that number is we select that button
+                                                // for example if the number is 8
+                                                // then we select the 8th button in the listO
+                jugadorActual = Player.O; // set the AI with O
+                buttons[index].Text = jugadorActual.ToString(); // show O on the button
+                buttons[index].BackColor = System.Drawing.Color.DarkSalmon; // change the background of the button dark salmon colour
+                buttons.RemoveAt(index); // remove that button from the list
+                Check(); // check if the AI won anything
+                IA.Stop(); // stop the AI timer
+            }
+        }
+        private void loadbuttons()
+        {
+            buttons = new List<Button> { a1, a2, a3, b1, b2, b3, c1, c2, c3 };
+        }
+        private void resetGame()
+        {
+            foreach (Control X in this.Controls)
+            {
+                if (X is Button && X != playGame && X != reinicio/**&& X.Tag == "play"**/)
+                {
+                    ((Button)X).Enabled = true; // change them all back to enabled or clickable
+                    ((Button)X).Text = "?"; // set the text to question mark
+                    ((Button)X).BackColor = default(Color); // change the background colour to default button colors
+                }
+            }
+
+            loadbuttons();
+        }
+        private void Check()
+        {
+            if (a1.Text == "X" && a2.Text == "X" && a3.Text == "X"
+               || b1.Text == "X" && b2.Text == "X" && b3.Text == "X"
+               || c1.Text == "X" && c2.Text == "X" && c3.Text == "X"
+               || a1.Text == "X" && b1.Text == "X" && c1.Text == "X"
+               || a2.Text == "X" && b2.Text == "X" && c2.Text == "X"
+               || a3.Text == "X" && b3.Text == "X" && c3.Text == "X"
+               || a1.Text == "X" && b2.Text == "X" && c3.Text == "X"
+               || a3.Text == "X" && b2.Text == "X" && c1.Text == "X")
+            {
+                // if any of the above conditions are met
+                IA.Stop(); //stop the timer
+                MessageBox.Show(nombre + " Wins"); // show a message to the player
+                playerWins++; // increase the player wins 
+                txtPlayer.Text = (nombre + " Wins - " + playerWins); // update player label
+                resetGame(); // run the reset game function
+            }
+            // below if statement is for when the AI wins the game
+            else if (a1.Text == "O" && a2.Text == "O" && a3.Text == "O"
+                   || b1.Text == "O" && b2.Text == "O" && b3.Text == "O"
+                   || c1.Text == "O" && c2.Text == "O" && c3.Text == "O"
+                   || a1.Text == "O" && b1.Text == "O" && c1.Text == "O"
+                   || a2.Text == "O" && b2.Text == "O" && c2.Text == "O"
+                   || a3.Text == "O" && b3.Text == "O" && c3.Text == "O"
+                   || a1.Text == "O" && b2.Text == "O" && c3.Text == "O"
+                   || a3.Text == "O" && b2.Text == "O" && c1.Text == "O")
+            {
+
+                // if any of the conditions are met above then we will do the following
+                // this code will run when the AI wins the game
+                IA.Stop(); // stop the timer
+                MessageBox.Show("Michi Wins"); // show a message box to say computer won
+                IAWins++; // increase the computer wins score number
+                txtMichi.Text = "Michi wins - " + IAWins; // update the label 2 for computer wins
+                resetGame(); // run the reset game
+            }
+
+        }
+        
+        private void iniciarJuego(object sender, EventArgs e)
+        {
+
+            if (namePlayer.Text == "")
+            {
+                MessageBox.Show("¿Contra quien voy a jugar?", "Ingresa un nombre", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (namePlayer.Text != "")
+            {
+                nombre = namePlayer.Text;
+                txtPlayer.Text = (nombre + " Wins - " + playerWins);
+                reinicio.Enabled = true;
+                resetGame();
+            }
+        }
+        private void reiniciarJuego(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+    }
+}
+
