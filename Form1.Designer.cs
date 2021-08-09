21bf20c371873b173e0a72cc256bfab065629ff7
xochitlcruz03 <64930479+xochitlcruz03@users.noreply.github.com>
2021-08-08 22:19:55 -0500

    Xo >:v

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,6 +47,8 @@ namespace JuguemosGato
             this.label1 = new System.Windows.Forms.Label();
             this.namePlayer = new System.Windows.Forms.TextBox();
             this.IA = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // a1
@@ -200,12 +202,35 @@ namespace JuguemosGato
             this.IA.Interval = 1000;
             this.IA.Tick += new System.EventHandler(this.IAmoves);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(327, 303);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(327, 337);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::JuguemosGato.Properties.Resources.back;
             this.ClientSize = new System.Drawing.Size(576, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.namePlayer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPlayer);
@@ -249,6 +274,8 @@ namespace JuguemosGato
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox namePlayer;
         private System.Windows.Forms.Timer IA;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
